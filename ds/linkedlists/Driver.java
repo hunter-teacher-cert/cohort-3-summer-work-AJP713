ade7f856ac43f3ce20875906ad43f60f382c1ec0
Adam Prado <64868729+AJP713@users.noreply.github.com>
2022-07-15 15:32:21 +0000

    linked list

---
+++ ds/linkedlists/Driver.java
@@ -0,0 +1,15 @@
+public class Driver{
+  public static void main(String[] args) {
+    LinkedList l = new LinkedList();
+
+    l.add("last");
+    l.add("next");
+     l.add("another");
+    l.add("first");
+    System.out.println(l);
+   // System.out.println(l.get(0));
+    // System.out.println(l.get(1));
+     System.out.println(l.get(3));
+  }
+}
+
