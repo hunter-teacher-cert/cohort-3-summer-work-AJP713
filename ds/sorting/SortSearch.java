1925ffeee6e07cc97eb0c5382bd8167602ea8825
Adam Prado <64868729+AJP713@users.noreply.github.com>
2022-07-21 18:08:40 +0000

    scaffolding

--- ds/sorting/SortSearch.java
+++ ds/sorting/SortSearch.java
@@ -1,71 +1,69 @@
 import java.io.*;
 import java.util.*;
 
-/*
-Sort Project:
 
-Part 1:  (BASIC)
-  1. Analyze the two constructors - try to figure out how they work.- DONE
-  2. Compile and run the program (SortSearchDriver.java and SortSearch.java) and confirm
-  the behavior of the constructors.- DONE
-  
-Part 2: (BASIC)
-  1. Read the description of findSmallestIndex and complete the method.- DONE
-  2. Uncomment the lines in SortSearchDriver to test.- DONE
-  
-Part 3: (INTERMEDIATE)
-  1. Complete the sort method - read comments for description - DONE
-  2. Uncomment the lines in SortSearchDriver to test. - DONE
-Search Project:
+/*
+  Sort Project:
+  Part 1:  (BASIC)
+  1. Analyze the two constructors - try to figure out how they work.
+  2. Compile and run the program (SortProjectDriver.java and SortProject.java) and confirm
+  the behavior of the constructors.
+  Part 2: (BASIC)
+  1. Read the description of findSmallestIndex and complete the method.
+  2. Uncomment the lines in SortProjectDriver to test.
+  Part 3: (INTERMEDIATE)
+  1. Complete the sort method - read comments for description
+  2. Uncomment the lines in sortProjectDriver to test.
+  Search Project:
   1. Complete the linear search (BASIC)
   2. Complete the binary search (Intermediate)
   3. Complete the recursive version of binary search (Advanced)
 */
 
-public class SortSearch{
+public class SortSearch {
 
     /* Sort project starts here */
-    
+
     /* Instance Variables */
     private ArrayList<Integer> data;  // to store the data
-    
-    private Random r; //variable to hold a randomly generated object.
 
-    
-    public SortSearch()//constructor that has no parameters
-    {
-    	data = new ArrayList<Integer>();//initializes the instance variable "data"
-    	r = new Random();//initializes the instance variable "r".
-    	for (int i=0; i<15; i++) //default size is 15
-      {
-    	  data.add(r.nextInt(20)); //the parameter "20" fills with random values between 0 and 19
-    	}
-	
+    private Random r;
+
+
+    public SortSearch() {
+      data = new ArrayList<Integer>();
+      r = new Random();
+      for (int i = 0; i < 15; i++) {
+        data.add(r.nextInt(20));
+      }
+
     }
-    
-    public SortSearch(int size)//overloaded constructor that takes in an int "size"
-    {
-    	data = new ArrayList<Integer>();//initializes the instance variable "data"
-    	r = new Random();//initializes the instance variable "r".
-    	for (int i=0; i<size; i++)//size is provided by the user through through the varialbe "size"
-      {
-    	  data.add(r.nextInt(20));//the parameter "20" fills with random values between 0 and 19
-    	}
-	
+
+    public SortSearch(int size) {
+      data = new ArrayList<Integer>();
+      r = new Random();
+      for (int i = 0; i < size; i++) {
+        data.add(r.nextInt(20));
+      }
+
     }
 
     /* Convenience function to get data out of the ArrayList from the driver */
-    public int get(int index){
-	    return this.data.get(index);
+    public int get(int index) {
+      return this.data.get(index);
     }
-    
 
+    /* Convenience function to get the size of the ArrayList from the driver.
+    */
+    public int size() {
+      return data.size();
+    }
 
     /*
       return the index of the smallest data idem from index start to the end
       of the ArrayList. If there are multiple of the smallest value,
       return any of them.
-      
+
       Example, if the arraylist has:
       5,3,10,6,8
       if start was 2 (start at index 2, value 10) then it would return 3
@@ -73,19 +71,16 @@ public class SortSearch{
       smallest value from start to end
       On the otherh and, if start was 0, then the method would
       return 1 since the value 3 is in index 1 and that is the smallest.
-      
+
     */
-    public int findSmallestIndex(int start)
-    {
-    	int smallIndex = start;
-      for(int i = start; i < data.size(); i++)
-      {
-        if(data.get(i) < data.get(smallIndex))
-        {
+    public int findSmallestIndex(int start) {
+      int smallIndex = start;
+      for (int i = start; i < data.size(); i++) {
+        if ( data.get(i) < data.get(smallIndex)) {
           smallIndex = i;
         }
-      }	//ends the for loop
-    	return smallIndex;
+      }
+      return smallIndex;
     }
 
 
@@ -96,94 +91,122 @@ public class SortSearch{
        Loop a variable that represents the ArrayList index from
        0 to the end of the ArrayList.
          For each index, find the smallest from that Location
-	 to the end of the array and swap it with that index.
-	 
-       
+      to the end of the array and swap it with that index.
+
+
     */
-    public void sort()
-    {
-      for(int i = 0; i < data.size(); i++)
-      {
-        int smallest = findSmallestIndex(i); //we call the method "findSmallestIndex, which finds the index that contains the smallest element(value) and stores the index (#) of that smallest element in the variable "smallest" which outside the arraylist.
-        
-        int temp = data.get(smallest); //takes the value of the element in smallest and saves it to a temporary variable, outside the arraylist, called "temp".
-        
-        data.set(smallest,data.get(i)); //gets the value of the element at position "i" and moves it (overwrites the data) at smallest.
-       
-        data.set(i,temp); //sets the value of the element at index i to the value currently in the variable temp.
-        
-      } // ends the for loop
+    public void sort() {
+      for (int i = 0; i < data.size(); i++) { //iterating the start position of find smallest index
+        int smallindex = findSmallestIndex(i); //sets the variable to index of smallest element
+        swapElements(data, smallindex, i);
 
+        //SORT WITHOUT USING SWAPELEMENTS METHOD
+        // int smallval = data.get(smallindex); //set the variable to the value of the smallest element
+        // int startval = data.get(i); //sets the value of the start position
+        // data.set(i, smallval); //puts the smallest values at the start position
+        // data.set(smallindex, startval); //puts the start value at the smallest values position
+      }
 
-    } // ends the sort method
+    }
 
+    public static void swapElements(ArrayList<Integer> dataList, int index1, int index2) {
+      int index1val = dataList.get(index1);// initialize variables and use get and set methods
+      int index2val = dataList.get(index2);
+      dataList.set(index1, index2val);// replace value in index 1 with value in index 2
+      dataList.set(index2, index1val); //replace value in index2 with value in index 1
+    }
 
 
     /* Search project starts here */
-    
+
     /**
        performs a linear search. Returns the index of the first occurence of
        value in the ArrayList data or -1 if not found.
-       This works by starting at the first element and searching one element at a time 
+       This works by starting at the first element and searching one element at a time
        until either the element is found or you've looked at all the elements.
        This algorithm works on any ArrayList.
     */
-    public int linearSearch(int value)
-    {
-      for(int i = 0; i < data.size(); i++)
-      {
-        if (data.get(i) == value)
-        {
+    public int linearSearch(int value) {
+      for (int i = 0; i < data.size(); i++) {
+        if (data.get(i) == value ) {
           return i;
         }
-        
-      } // ends the for loop
-	
-	
-	    return 0; // replace this return
+      }
+      return -1; // replace this return
     }
-    
+
     /**
        Implement a binary search as specified by the comments
-       
+
        This algorithm only works on sorted ArrayLists.
     */
-    public int binarySearch(int value){
+    public int binarySearch(int value) {
 
-	// create assign variables  representing the high, low and middle indices 
-	// while we're not done:
-	//   if the item is at data.get(middle), return middle
-	//   otherwise, update high, low, and middle
+      //set firstIndex, lastIndex, and middleIndex
+      int firstIndex = 0;
+      int lastIndex = data.size() - 1;
+      int middleIndex = (firstIndex + lastIndex) / 2;
 
-	    return 0;
-	    
+      while (data.get(middleIndex) != value) {
+
+        //if middleIndexValue is < value
+        if (data.get(middleIndex) < value) {
+          firstIndex = middleIndex;
+        }
+        //else if middleIndexValue > value
+        else {
+          lastIndex = middleIndex;
+        }
+        middleIndex = (firstIndex + lastIndex) / 2;
+
+        if (firstIndex == middleIndex || lastIndex == middleIndex) {
+          return -1;
+        }
+      }
+
+      return middleIndex;
+
     }
-    
+
     /**
        Implement a RECURSIVE binary search as specified by the comments
-       
+
        This algorithm only works on sorted ArrayLists.
     */
 
-    public int binarySearchRecursive(int value, int lowIndex, int highIndex){
+    public int binarySearchRecursive(int value, int lowIndex, int highIndex) {
 
-	// refer to class discussion
-	
-	    return 0;
-	    
+      if (highIndex - lowIndex != 1) {
+
+        int middleIndex = (lowIndex + highIndex) / 2;
+
+        if (data.get(middleIndex) == value) {
+          return middleIndex;
+        }
+
+        if (data.get(middleIndex) < value) {
+
+          return binarySearchRecursive(value, middleIndex, highIndex);
+        }
+
+        return binarySearchRecursive(value, lowIndex, middleIndex);
+
+      }
+      return -1;
+
     }
-    
-	
-    public String toString(){
-	    return ""+data;
+
+
+    public String toString() {
+      return "" + data;
+    };
+
+
+    public void builtinSort() {
+      Collections.sort(data);
+
     }
 
 
-    public void builtinSort(){
-	    Collections.sort(data);
-	
-    }
-    
 
-    
 }
--- ds/sorting/SortSearchDriver.java
+++ ds/sorting/SortSearchDriver.java
@@ -15,23 +15,57 @@ public class SortSearchDriver {
 
 	int i;
 	i = ss.findSmallestIndex(0);
-  System.out.println("\nPrint results when started at index 0 in the array:\nss[smallestIndex] = value : data");
 	System.out.println("ss["+ i +"] = "+ss.get(i)+" : " + ss);
 
-	i = ss.findSmallestIndex(10);
-  System.out.println("\nPrint results when started at index 3 in the array:\nss[smallestIndex] = value : data");
-	 System.out.println("ss["+ i + "] = "+ss.get(i)+" : " + ss);
+  System.out.println();
+      
+	i = ss.findSmallestIndex(3);
+	System.out.println("ss["+ i + "] = "+ss.get(i)+" : " + ss);
 	
 	// Uncomment these to test part 3
-  System.out.println("\nPrint array:");
+  System.out.println();
+      
+      
 	System.out.println(ss);
-  System.out.println("\nPrint sorted array:");
 	ss.sort();
+  
 	System.out.println(ss);
-	
+
+    System.out.println("Linear search found value 5 at index " + ss.linearSearch(5));
+
+    System.out.println("Binary search found value 5 at index " + ss.binarySearch(5));
+
+      System.out.println("Binary search found value 35 at index " + ss.binarySearch(35));
+
+      System.out.println("Recursive Bin Search found value 5 at index " + ss.binarySearchRecursive(5, 0, ss.size()-1));
+
+            System.out.println("Recursive Bin Search found value 35 at index " + ss.binarySearchRecursive(35, 0, ss.size()-1));
 
 
-	      
+    int size = 1000;
 
+    long start,elapsed;
+	  start = System.currentTimeMillis();
+	    // put what you want to test here!
+    SortSearch test = new SortSearch(size);
+     //System.out.println(test); 
+    test.sort();
+     // System.out.println(test); 
+	  elapsed = System.currentTimeMillis() - start;
+	  System.out.println("Size: " + size + " Time: " + elapsed);
+
+     size = 1000;
+
+    
+	  start = System.currentTimeMillis();
+	    // put what you want to test here!
+    SortSearch test2 = new SortSearch(size);
+     //System.out.println(test); 
+    test2.sort();
+     // System.out.println(test); 
+	  elapsed = System.currentTimeMillis() - start;
+	  System.out.println("Size: " + size + " Time: " + elapsed);
+
+      
     }
 }
